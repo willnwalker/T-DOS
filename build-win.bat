576074aca8e4592dd74800da9bfab88015790674
William <willnwalker@outlook.com>
2015-04-26 13:04:37 -0700

    Build Environment Changes
    -Added ".xml" files and ".ini" files to ".gitignore" file
    -Removed Build Environment zip
    -Reflected "nasm" name change in build script
    -Added Hex file viewer to \tools
    -Renamed "nasm2" to "nasm"

--- build-win.bat
+++ build-win.bat
@@ -5,7 +5,7 @@ echo.
 
 echo Assembling OS Kernel...
 cd tools
-start nasm2 -O0 -f bin -o %~d0\T-DOS\bin\bootload.bin %~d0\T-DOS\src\bootload.asm
+start nasm -O0 -f bin -o %~d0\T-DOS\bin\bootload.bin %~d0\T-DOS\src\bootload.asm
 
 echo Adding bootsector to disk image...
 start partcopy %~d0\T-DOS\bin\bootload.bin %~d0\T-DOS\img\tdos.flp 0d 511d
